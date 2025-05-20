5ecb04e2df283bf67e67fb70a724ea34e044d4f8
niharika528-n <kavalasatyaniharika@gmail.com>
2025-05-20 17:47:54 +0530

    Add files via upload

--- db/db.js
+++ db/db.js
@@ -1,16 +1,11 @@
-import mysql from 'mysql2/promise';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const { Pool } = pg;
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
