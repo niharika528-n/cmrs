f63a41da6d8db417fe22992e37e6e2ab47e71518
niharika528-n <kavalasatyaniharika@gmail.com>
2025-05-20 18:45:53 +0530

    Delete routes directory

--- routes/materials.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getMaterials, recommendMaterials } from '../controllers/materialController.js';
-
-const router = express.Router();
-
-router.get('/materials', getMaterials);
-router.post('/recommend', recommendMaterials);
-
-export default router;
